705a4b217db84b7745813652d164a390c627bcdb
TDavidTouitou <touitou@et.esiea.fr>
2020-11-27 15:46:16 +0100

    Implémentation de features (écran de connexion login + password)

--- app/src/main/java/com/example/projetandroid4a/domain/usecase/GetUserUseCase.kt
+++ app/src/main/java/com/example/projetandroid4a/domain/usecase/GetUserUseCase.kt
@@ -6,7 +6,7 @@ import com.example.projetandroid4a.domain.entity.User
 class GetUserUseCase(
     private val userRepository: UserRepository
 ) {
-    suspend fun invoke(email: String) : User{
+    suspend fun invoke(email: String) : User?{
         return userRepository.getUser(email)
     }
 }
